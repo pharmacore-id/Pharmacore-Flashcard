7560367aa5abb74dddc352449c45b27b6f71fe6e
pharmacore-id <adm.pharmacore@gmail.com>
2026-06-17 01:13:39 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,75 @@
 
+async function loadTopics(){
+
+  const res =
+    await fetch(
+      API_URL + "?action=getTopics"
+    );
+
+  const data =
+    await res.json();
+
+  const container =
+    document.getElementById(
+      "topicsContainer"
+    );
+
+  container.innerHTML = "";
+
+  data.topics.forEach(topic=>{
+
+    const block =
+      document.createElement("div");
+
+    block.className = "topic-block";
+
+    let html = `
+      <h2>${topic.mainTopic}</h2>
+    `;
+
+    topic.subTopics.forEach(sub=>{
+
+      html += `
+        <div
+          class="subtopic-card"
+          onclick="
+            startTopic(
+              '${topic.mainTopic}',
+              '${sub}'
+            )
+          ">
+          ${sub}
+        </div>
+      `;
+
+    });
+
+    block.innerHTML = html;
+
+    container.appendChild(block);
+
+  });
+
+}
+
+function startTopic(
+  mainTopic,
+  subTopic
+){
+
+  localStorage.setItem(
+    "mainTopic",
+    mainTopic
+  );
+
+  localStorage.setItem(
+    "subTopic",
+    subTopic
+  );
+
+  location.href =
+    "review.html";
+
+}
+
+loadTopics();
